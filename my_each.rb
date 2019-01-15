a021d9e2928bc7cbcc91ed754643b0fb3befa4a7
ebuchheim <emma.buchheim@gmail.com>
2019-01-15 12:07:56 -0800

    write a method using yield in place of each

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  how_long = array.length
+  i = 0
+  while i < how_long
+    yield(array[i])
+    i += 1
+  end
+  array
 end
